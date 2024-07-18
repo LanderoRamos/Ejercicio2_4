70cf3ca0e3f34ee257c4152beaf6b5d20d6d7fc5
LanderoRamos <dennislandero40@gmail.com>
2024-07-17 23:30:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio2_4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ejercicio2_4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnIngresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnIngresar = (Button) findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent prueba = new Intent(getApplicationContext(), ActivityFirma.class);
+                startActivity(prueba);
+            }
+        });
+    }
+}
